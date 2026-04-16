79ab114436353de338e3728dce420cde57eff02f
khansa-qonita <khansaqonita420@gmail.com>
2026-04-16 10:29:54 +0700

    menambahkan prosedur input dan validasi nPanjang

--- BinarySearch.cpp
+++ BinarySearch.cpp
@@ -5,4 +5,15 @@ int element[10];
 int nPanjang;
 int x;
 
+void input()
+{
+    while (true)
+    {
+        cout << "masukkan banyaknya elemen pada array (maksimal 10) : ";
+        cin >> nPanjang;
+
+        if (nPanjang <= 10)
+        {
+            break;
+        }
 
