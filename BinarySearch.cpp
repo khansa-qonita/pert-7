2d477553b73efc04771a4c616de1045afe2055e9
khansa-qonita <khansaqonita420@gmail.com>
2026-04-16 10:38:54 +0700

    menambahkan logika input data ke dalam array element sesuai jumlah nPanjang

--- BinarySearch.cpp
+++ BinarySearch.cpp
@@ -26,5 +26,14 @@ void input()
     cout << "  Masukkan Elemen Array  ";
     cout << "============\n";
 
-    
+    for (int i = 0; i < nPanjang; i++)
+    {
+        cout << "data ke- " << (i + 1) << " = ";
+        cin >> element[i];
+    }
+}
+
+void bubble
+
+
 
